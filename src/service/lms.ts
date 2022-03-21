f73686ca4613cc629044ee7c350169fa8127fefc
nulLeeKH <i_am@nulleekh.com>
2022-03-21 14:35:32 +0900

    add: src/service/lms.ts

---
+++ src/service/lms.ts
@@ -0,0 +1,64 @@
+import logger from '../util/logger'
+import { getLectureRepo, getBoardRepo, getEnrolmentRepo } from '../repository/lms'
+// import {} from "../type/lms";
+import { Board, Enrolment, Lecture } from '../domain/lms'
+
+class LmsService {
+  async addLecture(profId: number, title: string): Promise<Lecture | undefined> {
+    try {
+      return await getLectureRepo().add(profId, title)
+    } catch (e: any) {
+      logger.error(e.stack)
+    }
+    return undefined
+  }
+
+  async listLectureByProfId(profId: number): Promise<Lecture[] | undefined> {
+    try {
+      return await getLectureRepo().listByProfId(profId)
+    } catch (e: any) {
+      logger.error(e.stack)
+    }
+    return undefined
+  }
+
+  async addBoard(lectureId: number, title: string, content: string): Promise<Board | undefined> {
+    try {
+      return await getBoardRepo().add(lectureId, title, content)
+    } catch (e: any) {
+      logger.error(e.stack)
+    }
+    return undefined
+  }
+
+  async listBoardByLectureId(lectureId: number): Promise<Board[] | undefined> {
+    try {
+      return await getBoardRepo().listByLectureId(lectureId)
+    } catch (e: any) {
+      logger.error(e.stack)
+    }
+    return undefined
+  }
+
+  async addEnrolment(lectureId: number, stdtId: number): Promise<Enrolment | undefined> {
+    try {
+      return await getEnrolmentRepo().add(lectureId, stdtId)
+    } catch (e: any) {
+      logger.error(e.stack)
+    }
+    return undefined
+  }
+
+  async listEnrolmentByStdtId(stdtId: number): Promise<Enrolment[] | undefined> {
+    try {
+      return await getEnrolmentRepo().listByStdtId(stdtId)
+    } catch (e: any) {
+      logger.error(e.stack)
+    }
+    return undefined
+  }
+}
+
+const lmsService = new LmsService()
+
+export { lmsService }
