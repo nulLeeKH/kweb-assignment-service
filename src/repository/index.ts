fa49722e6acdcf0156ca0a7eb62c2aa833a80564
nulLeeKH <i_am@nulleekh.com>
2022-03-27 21:40:01 +0900

    fix: repositories in lms are not refreshed

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -1,8 +1,10 @@
-import {
-  refreshUserRepo,
-  refreshTokenRepo} from './auth'
+import { refreshUserRepo, refreshTokenRepo } from './auth'
+import { refreshLectureRepo, refreshBoardRepo, refreshEnrolmentRepo } from './lms'
 
 export function refreshAllRepo(): void {
   refreshUserRepo()
   refreshTokenRepo()
+  refreshLectureRepo()
+  refreshBoardRepo()
+  refreshEnrolmentRepo()
 }
