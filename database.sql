590dd5eb5d4af6d0cad36dc987872430dfe176f8
nulLeeKH <i_am@nulleekh.com>
2022-03-28 12:39:46 +0900

    rfc: use index in database

--- database.sql
+++ database.sql
@@ -12,35 +12,45 @@ CREATE TABLE `user` (
   `type` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `changed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `deleted_at` timestamp NOT NULL DEFAULT 0);
+  `deleted_at` timestamp NOT NULL DEFAULT 0
+  );
+CREATE INDEX IDX_USER_USERNAME ON user(username);
 
 CREATE TABLE `token` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `serial` char(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `deleted_at` timestamp NOT NULL DEFAULT DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 12 HOUR));
+  `deleted_at` timestamp NOT NULL DEFAULT DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 12 HOUR)
+  );
 ALTER TABLE token ADD FOREIGN KEY (user_id) REFERENCES user(id);
+CREATE INDEX IDX_TOKEN_SERIAL ON token(serial);
 
 CREATE TABLE `lecture` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `prof_id` int NOT NULL,
-  `title` varchar(255) NOT NULL);
+  `title` varchar(255) NOT NULL
+   );
 ALTER TABLE lecture ADD FOREIGN KEY (prof_id) REFERENCES user(id);
 
 CREATE TABLE `board` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `lecture_id` int NOT NULL,
   `title` varchar(255) NOT NULL,
-  `content` longtext NOT NULL);
+  `content` longtext NOT NULL
+   );
 ALTER TABLE board ADD FOREIGN KEY (lecture_id) REFERENCES lecture(id);
+CREATE INDEX IDX_BOARD_LECTURE ON board(lecture_id);
 
 CREATE TABLE `enrolment` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `lecture_id` int NOT NULL,
-  `stdt_id` int NOT NULL);
+  `stdt_id` int NOT NULL
+  );
 ALTER TABLE enrolment ADD FOREIGN KEY (lecture_id) REFERENCES lecture(id);
 ALTER TABLE enrolment ADD FOREIGN KEY (stdt_id) REFERENCES user(id);
+CREATE INDEX IDX_ENROLMENT_LECTURE ON enrolment(lecture_id);
+CREATE INDEX IDX_ENROLMENT_STDT ON enrolment(stdt_id);
 
 
 use mysql;
